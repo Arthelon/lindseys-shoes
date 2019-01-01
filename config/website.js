1b05ef8008dde49d91eb1cdd890e346b24b82aa4
Daniel Hsing <hsing.daniel@gmail.com>
2019-01-01 15:38:33 -0500

    feat: initial commit
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

---
+++ config/website.js
@@ -0,0 +1,19 @@
+module.exports = {
+    pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+    siteTitle: "Lindsey's Shoes", // Navigation and Site Title
+    siteTitleAlt: "Lindsey's Shoes", // Alternative Site title for SEO
+    siteUrl: "https://lindseys-shoes.netlify.com", // Domain of your site. No trailing slash!
+    siteLanguage: "en", // Language Tag on <html> element
+    siteLogo: "/logos/logo-1024.png", // Used for SEO and manifest
+    siteDescription: "Lindsey's Shoes",
+    author: "Arthelon", // Author for schemaORGJSONLD
+
+    // siteFBAppID: '123456789', // Facebook App ID - Optional
+    userTwitter: "@test", // Twitter Username
+    ogSiteName: "test", // Facebook Site Name
+    ogLanguage: "en_US", // Facebook Language
+
+    // Manifest and Progress color
+    themeColor: "#b710a1",
+    backgroundColor: "#350d27"
+};
