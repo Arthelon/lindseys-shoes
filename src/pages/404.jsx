735f12af7581296b596e5254e48c252d9b2c77e3
Daniel Hsing <hsing.daniel@gmail.com>
2019-01-04 15:46:00 -0500

    feat: style 404 page
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import Layout from '../components/Layout';
+import React from "react";
+import Layout from "../components/Layout";
+import { Link } from "gatsby";
+import styled from "react-emotion";
+
+const Wrapper = styled.div`
+    text-align: center;
+    margin-top: 6rem;
+    p {
+        margin-top: 3rem;
+        font-size: 2rem;
+    }
+    a {
+        font-size: 1.5rem;
+    }
+`;
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <Wrapper>
+            <h1>NOT FOUND</h1>
+            <p>You just hit a route that doesn&#39;t exist. </p>
+            <Link to="/">Return Home</Link>
+        </Wrapper>
+    </Layout>
 );
 
 export default NotFoundPage;
