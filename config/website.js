78aa1185972e5a9f44c4953d83b3dfb3725c4aac
Daniel Hsing <hsing.daniel@gmail.com>
2019-01-21 13:57:20 -0500

    feat: update website config
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- config/website.js
+++ config/website.js
@@ -1,11 +1,11 @@
 module.exports = {
     pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-    siteTitle: "Lindsey's Shoes", // Navigation and Site Title
-    siteTitleAlt: "Lindsey's Shoes", // Alternative Site title for SEO
+    siteTitle: "Jay Walking Co.", // Navigation and Site Title
+    siteTitleAlt: "Jay Walking Co.", // Alternative Site title for SEO
     siteUrl: "https://lindseys-shoes.netlify.com", // Domain of your site. No trailing slash!
     siteLanguage: "en", // Language Tag on <html> element
     siteLogo: "/logos/logo-1024.png", // Used for SEO and manifest
-    siteDescription: "Lindsey's Shoes",
+    siteDescription: "Jay Walking Co.",
     siteEmail: "lindseys-shoes@gmail.com",
     author: "Arthelon", // Author for schemaORGJSONLD
 
